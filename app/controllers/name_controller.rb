62ccbceaa25ccadfde972974377510628650d4b7
Muhammet DILEK <dilekmuhammet@gmail.com>
2011-11-30 08:16:18 +0200

    Issue #3 name api response structure changed

--- app/controllers/name_controller.rb
+++ app/controllers/name_controller.rb
@@ -1,13 +1,19 @@
 class NameController < ApplicationController
-  respond_to :html, :xml, :json
-
   def index
     show
   end
 
   def show
-    @name=Name.find_by_name(params[:name])
-    respond_with([@name])
+    @name=Name.select("name, gender").find_by_name(params[:name])
+    if @name!=nil
+      @name_json=["success" =>true, "name" => @name]
+      render json: @name_json
+    else
+      @name_json=["success" => false]
+      render json: @name_json
+    end
+
+
   end
 
 end
